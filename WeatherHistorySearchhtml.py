29abf3b0108845e739f8afefa1af9369dfc4257c
alta_jp24 <178458424+alta-jp24@users.noreply.github.com>
2025-01-18 18:06:33 +0900

    サイトを取得するコードと出力

---
+++ WeatherHistorySearchhtml.py
@@ -0,0 +1,9 @@
+import requests
+
+url = 'https://www.data.jma.go.jp/stats/etrn/view/daily_a1.php?prec_no=44&block_no=1001&year=2024&month=3&day=15&view='
+response = requests.get(url)
+response.encoding = response.apparent_encoding 
+
+# テキストファイルに保存する
+with open('WeatherHistorySearch.html', 'w', encoding='utf-8') as file:
+    file.write(response.text)
