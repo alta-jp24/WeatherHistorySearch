2e6524ba6f575ada410730f2213a5925df2fb584
alta_jp24 <178458424+alta-jp24@users.noreply.github.com>
2025-01-18 18:49:12 +0900

    修正

---
+++ WeatherHistorySearch_BeautifulSoup.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+
+# HTMLデータの読み込み
+html_file = 'WeatherHistorySearch.html'  # 保存したHTMLファイル名
+with open(html_file, 'r', encoding='utf-8') as file:
+    html_data = file.read()
+
+
+# レスポンスの HTML から BeautifulSoup オブジェクトを作る
+soup = BeautifulSoup(html_data, 'html.parser')
+
+# <tr>タグの文字列を取得する
+# <tr class="mtx" style="text-align:right;">
+rows = soup.find_all('tr', {'class': 'mtx', 'style': 'text-align:right;'})
+
+
+# 指定の日付
+target_day = '15'
+
+# title タグの文字列を取得する
+target_row = soup.find('a', string=target_day).find_parent('tr')
+
+if target_row:
+    values = [td.text.strip() for td in target_row.find_all('td')]
+
+    result = f"{values[0]}日, {values[1]}mm, {values[4]}℃, {values[5]}℃, {values[6]}℃"
+
+    print(result)
+
+        # テキストファイルに保存する
+    with open('WeatherHistorySearchSoup.html', 'w', encoding='utf-8') as file:
+        file.write(str(result))
